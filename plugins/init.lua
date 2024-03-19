8e4d147776ff346a2a7595468cb74471a92b9184
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-03-19 18:28:15 +0100

    Adapt which-key

--- plugins/init.lua
+++ plugins/init.lua
@@ -327,6 +327,16 @@ local plugins = {
       require("yanky").setup(opts)
     end
   },
+
+  -- Only load whichkey after all the gui
+  {
+    "folke/which-key.nvim",
+    keys = { "<leader>", "<c-r>", "<c-w>", '"', "'", "`", "c", "v", "g" },
+    cmd = "WhichKey",
+    config = function(_, opts)
+      require("which-key").setup(opts)
+    end,
+  },
 }
 
 return plugins
