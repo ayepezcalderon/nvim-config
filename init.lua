bc47cb8c38d8f2cffead5e55a384bb2bbaafa571
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-03-20 14:51:12 +0100

    bootstrap lazy.nvim

--- init.lua
+++ init.lua
@@ -1,3 +1,24 @@
+-- remap leader key
+vim.g.mapleader = " "local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- load plugins
+require("lazy").setup({
+  { import = "plugins" },
+}, lazy_config)
+
 local opt = vim.opt
 local o = vim.o
 
