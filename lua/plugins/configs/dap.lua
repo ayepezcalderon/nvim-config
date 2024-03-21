a957d45a24f44fa4058ebe5e09ceae25c6627f4a
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-03-21 01:01:15 +0100

    Refactor dap stuff

--- lua/plugins/configs/dap.lua
+++ lua/plugins/configs/dap.lua
@@ -2,6 +2,10 @@
 local function _config (_, opts)
   -- Set colors with one of the util functions
   require("plugins._utils.dap").color_hls()
+
+  -- Telescope functionality
+  -- Here for lazy loading only on dap demand, not on telescope demand
+  require("telescope").load_extension("dap")
 end
 
 return {
--- lua/plugins/utils/dap.lua
+++ lua/plugins/utils/dap.lua
@@ -1,8 +1,23 @@
 local M = {}
 
--- Telescope functionality
--- Here for lazy loading only on dap demand, not on telescope demand
-require("telescope").load_extension("dap")
+--- Add keymaps to current buffer to easily close it with q and <ESC> in normal mode
+function M.set_buf_easy_close_mappings()
+  vim.keymap.set('n', 'q', "<cmd>:q<CR>", { buffer = 0 })
+  vim.keymap.set('n', '<ESC>', "<cmd>:q<CR>", { buffer = 0 })
+end
+
+--- Custom dap repl toggle function. Can enter repl in insert mode when toggle opens
+function M.custom_dap_repl_toggle(insert, ...)
+  if not require("dap").repl.close({ mode = 'toggle' }) then
+    require("dap").repl.open(...)
+    -- Move to repl
+    vim.cmd(":wincmd p")
+    -- Enter insert mode if required
+    if insert then
+      vim.cmd("startinsert")
+    end
+  end
+end
 
 
 -- Colors for the dap highlight groups
