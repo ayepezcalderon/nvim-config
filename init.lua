a8b63c89e048b83233b0e5c8ea2a2ad4f082831d
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-04-05 01:40:27 +0200

    Add stuff from nvchad core init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,20 @@
 -- remap leader key
-vim.g.mapleader = " "local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+vim.g.mapleader = " "
+
+-- disable some default providers
+for _, provider in ipairs { "node", "perl", "python3", "ruby" } do
+  vim.g["loaded_" .. provider .. "_provider"] = 0
+end
+
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+vim.env.PATH = vim.fn.stdpath "data" .. "/mason/bin" .. (is_windows and ";" or ":") .. vim.env.PATH
 
 -- load core
 require("core")
 
 -- bootstrap lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -7,3 +7,10 @@ vim.api.nvim_create_autocmd({"BufRead", "BufNewFile", "BufEnter", "BufWinEnter"}
   callback = function() opt.colorcolumn = '88' end,
 })
 
+-- dont list quickfix buffers
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "qf",
+  callback = function()
+    vim.opt_local.buflisted = false
+  end,
+})
