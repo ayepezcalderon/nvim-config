19da43d3344dddc259779a3766774657e4fd76ad
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-04-07 12:37:41 +0200

    bufferline conform to plugin structure

--- lua/plugins/bufferline/init.lua
+++ lua/plugins/bufferline/init.lua
@@ -1,11 +1,15 @@
+------ OPTS -------
+local _opts = {
+  options = {
+    mode = "tabs",
+    separator_style = "slant",
+  },
+}
+
+------ RETURN -------
 return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+  opts = _opts
 }
