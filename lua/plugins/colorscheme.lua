dd6c8d2c88d01fa407a35542b5c58d2726a880ac
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-04-17 17:10:25 +0200

    put colorschemes in __colorschemes__. Specify current colorscheme(.lua) as a symbolic link to the desire colorscheme in __colorschemes__

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,1 @@
--- Use this file for any colorschemes that you wish to install.
--- Comes with catppuccin by default.
-return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy=false,
-    priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme catppuccin]])
-    end,
-  },
-}
-
+__colorschemes__/catppuccin.lua
