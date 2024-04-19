e13a039bb4c1c876d8e6f233271b0c4e16ad95aa
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-04-19 11:08:17 +0200

    legibility

--- lua/plugins/gitsigns/mappings.lua
+++ lua/plugins/gitsigns/mappings.lua
@@ -11,21 +11,25 @@ M.load = function (bufnr)
   end
 
   -- Navigation through hunks
-  map('n', ']c', function()
-    if vim.wo.diff then return ']c' end
-    vim.schedule(function() gs.next_hunk() end)
-    return '<Ignore>'
-  end, {
+  map('n', ']c',
+    function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end,
+    {
       expr = true,
       desc = "Jump to next hunk",
     }
   )
 
-  map('n', '[c', function()
-    if vim.wo.diff then return '[c' end
-    vim.schedule(function() gs.prev_hunk() end)
-    return '<Ignore>'
-  end, {
+  map('n', '[c',
+    function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end,
+    {
       expr = true,
       desc = "Jump to prev hunk",
     }
