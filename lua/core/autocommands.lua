a55a7addd46d320caa65b5d599ccf1ebd6b0825b
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-04-21 21:28:20 +0200

    improve setting colorcolumn in python by doing it in after/ftplugin instead of as an autocmd

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,12 +1,3 @@
-local opt = vim.opt
-
-vim.api.nvim_create_augroup('Colorcolumns', { clear = true })
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile", "BufEnter", "BufWinEnter"}, {
-  pattern = {"*.py"},
-  group = 'Colorcolumns',
-  callback = function() opt.colorcolumn = '88' end,
-})
-
 -- dont list quickfix buffers
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "qf",
