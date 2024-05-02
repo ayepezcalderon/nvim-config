e6398f9fd96edc13114ba36f05491e6ca2e2fe49
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-05-02 10:49:06 +0200

    improve treesitter-textobjects mappings

--- lua/plugins/treesitter-text-objects/mappings.lua
+++ lua/plugins/treesitter-text-objects/mappings.lua
@@ -30,8 +30,8 @@ M.textobjects = {
       ["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
       ["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
 
-      ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
-      ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+      ["ab"] = { query = "@class.outer", desc = "Select outer part of a class" },
+      ["ib"] = { query = "@class.inner", desc = "Select inner part of a class" },
     },
   },
   swap = {
@@ -49,6 +49,7 @@ M.textobjects = {
     enable = true,
     set_jumps = true, -- whether to set jumps in the jumplist
     goto_next_start = {
+      ["]a"] = { query = "@parameter.outer", desc = "Next parameter/argument start" },
       ["]f"] = { query = "@call.outer", desc = "Next function call start" },
       ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
       ["]b"] = { query = "@class.outer", desc = "Next class start" },
@@ -56,6 +57,7 @@ M.textobjects = {
       ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
     },
     goto_next_end = {
+      ["]A"] = { query = "@parameter.outer", desc = "Next parameter/argument end" },
       ["]F"] = { query = "@call.outer", desc = "Next function call end" },
       ["]M"] = { query = "@function.outer", desc = "Next method/function def end" },
       ["]B"] = { query = "@class.outer", desc = "Next class end" },
@@ -63,6 +65,7 @@ M.textobjects = {
       ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
     },
     goto_previous_start = {
+      ["[a"] = { query = "@parameter.outer", desc = "Prev parameter/argument call start" },
       ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
       ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
       ["[b"] = { query = "@class.outer", desc = "Prev class start" },
@@ -70,6 +73,7 @@ M.textobjects = {
       ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
     },
     goto_previous_end = {
+      ["[A"] = { query = "@parameter.outer", desc = "Prev parameter/argument call end" },
       ["[F"] = { query = "@call.outer", desc = "Prev function call end" },
       ["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
       ["[B"] = { query = "@class.outer", desc = "Prev class end" },
