c9c0552b629e32867f9d5260dbb59085f42ee5d4
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-05-28 18:27:52 +0200

    add copilot chat

---
+++ lua/plugins/copilot-chat/init.lua
@@ -0,0 +1,31 @@
+---------- OPTS ------------
+local _opts = {
+  debug = true, -- Enable debugging
+
+  -- window = {
+  --   layout = 'float',
+  --   relative = 'cursor',
+  --   width = 1,
+  --   height = 0.4,
+  --   row = 1
+  -- },
+
+}
+
+---------- RETURN ------------
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    cmd = "Copilot",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    opts = _opts,
+    config = function (_, opts)
+      require("CopilotChat").setup(opts)
+      require("plugins.copilot-chat.mappings")
+    end,
+  },
+}
---
+++ lua/plugins/copilot-chat/mappings.lua
@@ -0,0 +1,33 @@
+local map = vim.keymap.set
+
+map(
+  "n",
+  "<leader>ccq",
+  function()
+    local input = vim.fn.input("Quick Chat: ")
+    if input ~= "" then
+      require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+    end
+  end,
+  { desc = "CopilotChat - Quick chat" }
+)
+
+map(
+  "n",
+  "<leader>cch",
+  function()
+    local actions = require("CopilotChat.actions")
+    require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+  end,
+  { desc = "CopilotChat - Help actions" }
+)
+
+map(
+  "n",
+  "<leader>ccp",
+  function()
+    local actions = require("CopilotChat.actions")
+    require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+  end,
+  { desc = "CopilotChat - Prompt actions" }
+)
