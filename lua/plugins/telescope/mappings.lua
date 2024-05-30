e1da07a05f1640fbca742d1550b8f7368e8a6aad
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-05-30 16:41:56 +0200

    telescope mappings for lsp symbols

--- lua/plugins/telescope/mappings.lua
+++ lua/plugins/telescope/mappings.lua
@@ -1,74 +1,86 @@
 local map = vim.keymap.set
 
 map(
-  {"n"},
+  { "n" },
   "<leader>ma",
   '<cmd> Telescope marks <CR>',
-  {desc="telescope bookmarks"}
+  { desc = "telescope bookmarks" }
 )
 map(
-  {"n"},
+  { "n" },
   "<leader>fb",
   '<cmd> Telescope buffers <CR>',
-  {desc="Find buffers"}
+  { desc = "Find buffers" }
 )
 map(
-  {"n"},
+  { "n" },
   "<leader>fh",
   '<cmd> Telescope help_tags <CR>',
-  {desc="Help page"}
+  { desc = "Help page" }
 )
 map(
-  {"n"},
+  { "n" },
   "<leader>cm",
   '<cmd> Telescope git_commits <CR>',
-  {desc="Git commits"}
+  { desc = "Git commits" }
 )
 map(
-  {"n"},
+  { "n" },
   "<leader>pt",
   '<cmd> Telescope terms <CR>',
-  {desc="Pick hidden term"}
+  { desc = "Pick hidden term" }
 )
 map(
-  {"n"},
+  { "n" },
   "<leader>gt",
   '<cmd> Telescope git_status <CR>',
-  {desc="Git status"}
+  { desc = "Git status" }
 )
 map(
-  {"n"},
+  { "n" },
   "<leader>fw",
   '<cmd> Telescope live_grep <CR>',
-  {desc="Live grep"}
+  { desc = "Live grep" }
 )
 map(
-  {"n"},
+  { "n" },
   "<leader>fz",
   '<cmd> Telescope current_buffer_fuzzy_find <CR>',
-  {desc="Find in current buffer"}
+  { desc = "Find in current buffer" }
 )
 map(
-  {"n"},
+  { "n" },
   "<leader>fo",
   '<cmd> Telescope oldfiles <CR>',
-  {desc="Find oldfiles"}
+  { desc = "Find oldfiles" }
 )
 map(
-  {"n"},
+  { "n" },
   "<leader>ff",
   '<cmd> Telescope find_files <CR>',
-  {desc="Find files"}
+  { desc = "Find files" }
 )
 map(
-  {"n"},
+  { "n" },
   "<leader>fa",
   '<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>',
-  {desc="Find all"}
+  { desc = "Find all" }
 )
 map(
-  {"n"},
+  { "n" },
   "<leader>fy",
   '<cmd> Telescope yank_history <CR>',
-  {desc="Yank history"}
+  { desc = "Yank history" }
+)
+map(
+  { "n" },
+  "<leader>fld",
+  '<cmd> Telescope lsp_document_symbols <CR>',
+  { desc = "Document/buffer LSP symbols" }
+)
+map(
+  { "n" },
+  "<leader>flw",
+  '<cmd> Telescope lsp_workspace_symbols <CR>',
+  { desc = "Workspace LSP symbols" }
 )
