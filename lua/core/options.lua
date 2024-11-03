2497700b2c196c2dbeb8c7a05c9a5d8d5942c51d
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-11-03 12:56:20 +0100

    add folding with treesitter

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -64,3 +64,6 @@ opt.updatetime = 250
 -- go to previous/next line with h,l,left arrow and right arrow
 -- when cursor reaches end/beginning of line
 opt.whichwrap:append("<>[]hl")
+
+-- Folding
+opt.foldlevel = 99
