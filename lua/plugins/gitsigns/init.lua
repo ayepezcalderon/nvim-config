c05c9f36344b9f63ce9cadbef2438d8515b66cf9
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-11-03 01:32:06 +0100

    centralize method of loading git plugins

--- lua/plugins/gitsigns/init.lua
+++ lua/plugins/gitsigns/init.lua
@@ -1,19 +1,5 @@
 ---------- INIT ------------
-local function _init()
-  -- load gitsigns only when a git file is opened
-  vim.api.nvim_create_autocmd({ "BufRead" }, {
-    group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
-    callback = function()
-      vim.fn.system("git -C " .. '"' .. vim.fn.expand("%:p:h") .. '"' .. " rev-parse")
-      if vim.v.shell_error == 0 then
-        vim.api.nvim_del_augroup_by_name("GitSignsLazyLoad")
-        vim.schedule(function()
-          require("lazy").load({ plugins = { "gitsigns.nvim" } })
-        end)
-      end
-    end,
-  })
-end
+local _init = require("utils").init_git_plugin("gitsigns.nvim", "GitSignsLazyLoad")
 
 ----------- OPTS ------------
 local map_on_demand = require("plugins.gitsigns.mappings")
--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -36,4 +36,23 @@ M.lazy_load = function(plugin)
   })
 end
 
+--- Function to assign to init field of lazy spec for plugins that should only load for git repos.
+---@param plugin string Name of plugin.
+---@param augroup string Name of augroup created during this process.
+M.init_git_plugin = function(plugin, augroup)
+  -- load gitsigns only when a git file is opened
+  vim.api.nvim_create_autocmd({ "BufRead" }, {
+    group = vim.api.nvim_create_augroup(augroup, { clear = true }),
+    callback = function()
+      vim.fn.system("git -C " .. '"' .. vim.fn.expand("%:p:h") .. '"' .. " rev-parse")
+      if vim.v.shell_error == 0 then
+        vim.api.nvim_del_augroup_by_name(augroup)
+        vim.schedule(function()
+          require("lazy").load({ plugins = { plugin } })
+        end)
+      end
+    end,
+  })
+end
+
 return M
