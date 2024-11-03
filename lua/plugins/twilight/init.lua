f1aa1511284a65de56f9660074e6154096c3b57b
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-11-03 02:40:19 +0100

    add Twilight

---
+++ lua/plugins/twilight/init.lua
@@ -0,0 +1,22 @@
+------------- CONFIG -------------
+local function _config(_, opts)
+  -- require("twilight.nvim").setup(opts)
+  -- mappings
+  require("plugins.twilight.mappings")
+end
+
+
+---------- RETURN -----------
+return {
+  "folke/twilight.nvim",
+  init = function()
+    -- Same conditions as tree-sitter
+    require("utils").lazy_load("twilight.nvim")
+  end,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  config = _config
+}
