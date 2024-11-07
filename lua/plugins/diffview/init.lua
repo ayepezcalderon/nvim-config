e13b4834cc07cbfd1b2585c7d29fe30e1a527da2
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-11-07 17:39:44 +0100

    fixed diffview

--- lua/plugins/diffview/init.lua
+++ lua/plugins/diffview/init.lua
@@ -1,6 +1,3 @@
----------- INIT ------------
-local _init = require("utils").init_git_plugin("diffview", "DiffviewLazyLoad")
-
 ---------- CONFIG ------------
 local _config = function(_, opts)
   require("diffview").setup(opts)
@@ -12,6 +9,5 @@ end
 return {
   "sindrets/diffview.nvim",
   ft = { "gitcommit", "diff" },
-  init = _init,
   config = _config
 }
