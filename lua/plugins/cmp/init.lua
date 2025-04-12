1b03bc2bb094de60bf958119dff595792593b874
Andrés Yépez Calderón <andresyepez966@gmail.com>
2025-04-12 14:04:05 +0200

    cmp only on demand

--- lua/plugins/cmp/init.lua
+++ lua/plugins/cmp/init.lua
@@ -4,7 +4,7 @@ local enabled = function()
   -- enable in dap buffers
   if package.loaded["cmp_dap"] then
     disabled = disabled
-      or (vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "prompt" and not require("cmp_dap").is_dap_buffer())
+        or (vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "prompt" and not require("cmp_dap").is_dap_buffer())
   else
     disabled = disabled or (vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "prompt")
   end
@@ -36,6 +36,7 @@ local function _opts()
   return {
     completion = {
       completeopt = "menu,menuone",
+      autocomplete = false,
     },
 
     window = {
@@ -84,19 +85,18 @@ local function _opts()
       ["<C-n>"] = cmp.mapping.select_next_item(),
       ["<C-d>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.close(),
       ["<CR>"] = cmp.mapping.confirm({
         behavior = cmp.ConfirmBehavior.Insert,
         select = true,
       }),
-      ["<Tab>"] = cmp.mapping(function(fallback)
+      ["<Tab>"] = cmp.mapping(function()
         if cmp.visible() then
           cmp.select_next_item()
         elseif require("luasnip").expand_or_jumpable() then
           vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
         else
-          fallback()
+          cmp.complete()
         end
       end, {
         "i",
