2a7ea772f766be0b531931ebfa238d5959aae328
Andrés Yépez Calderón <andresyepez966@gmail.com>
2025-04-12 19:19:42 +0200

    async false for format on save -> buggy

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -22,8 +22,10 @@ M.on_attach = function(client, bufnr)
   if config.lsp.format_on_save then
     if client.supports_method("textDocument/formatting") then
       vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup("FormatOnSave" .. bufnr, { clear = true }),
+        buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({ async = true })
+          vim.lsp.buf.format({ async = false, bufnr = bufnr })
         end,
       })
     end
