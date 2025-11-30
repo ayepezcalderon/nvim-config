2f0262dbdc744f5549d28a2d4e7daa930c30f5cb
Andrés Yépez Calderón <andres.yepezcalderon@tno.nl>
2025-11-30 23:44:54 +0100

    uv commands improvements

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -23,7 +23,13 @@ vim.api.nvim_buf_create_user_command(0, "UvRunTerm", function(opts)
   -- Send the command to the terminal
   local term_chan = vim.b.terminal_job_id
   vim.fn.chansend(term_chan, "uv run " .. file .. "\n")
+
+  -- Enter insert mode in the terminal
+  vim.cmd("startinsert")
 end, {
   nargs = "?",
   desc = "Run `uv run` on current file in a persistent terminal",
 })
+
+local map = vim.keymap.set
+map("n", "<F10>", ":UvRunTerm<CR>", { buffer = true, desc = "Run `uv run` in terminal" })
